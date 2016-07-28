fdef111fdbac23ed4e501f55766f967f9ba94cbc
Simeon Naydenov <moniNaydenov@gmail.com>
2016-07-28 18:47:39 +0300

    Bug #3574 - fix long course/assignment names

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016062100;
+$plugin->version   = 2016072800;
 $plugin->release   = "v3.1.0";       // User-friendly version number.
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2014041100;      // Requires this Moodle version!
