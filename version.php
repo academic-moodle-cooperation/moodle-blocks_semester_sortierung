1ce4fea87952c4b4d66b11aace1fd87507c0f681
Simeon Naydenov <moniNaydenov@gmail.com>
2019-05-08 00:45:38 +0300

    [UPDATE] #6059 - fix github issues

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018121000;
+$plugin->version   = 2018121001;
 $plugin->requires  = 2018120300;
 $plugin->component = 'block_semester_sortierung';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "v3.6.0";
+$plugin->release   = "v3.6.1";
